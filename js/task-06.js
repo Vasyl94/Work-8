e1637464270633c840f451048acba0f77d942a34
Vasyl94 <basyl94@icloud.com>
2023-04-24 16:38:09 +0300

    147

--- js/task-06.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.getElementById('validation-input');
-
-inputEl.addEventListener('blur', (event) => {
-    if (event.currentTarget.value.length == inputEl.dataset.length) {
-        inputEl.classList = 'valid'        
-    } else inputEl.classList = 'invalid';    
-});
--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.getElementById('font-size-control');
-const textEl = document.getElementById('text');
-
-inputEl.addEventListener('input', () => {
-    textEl.style.fontSize = `${Number(inputEl.value)}px`;
-});
-
